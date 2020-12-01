c51d22626eab70279ed5fa678c25f126fd5def8d
charodiy <daraihnatova@gmail.com>
2020-12-01 14:51:57 +0200

    added post service added comment service

---
+++ src/app/tab3/services/comment.service.ts
@@ -0,0 +1,15 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CommentService {
+
+    constructor(private httpClient: HttpClient) {
+    }
+
+    getAllComments(): any {
+        return this.httpClient.get<any[]>('https://jsonplaceholder.typicode.com/comments');
+    }
+}
